af505a2fc12aee8dade386df3803b26ba60ba04b
Marcelly Oliveira <160605874+Rayoliveir@users.noreply.github.com>
2024-09-03 14:00:05 +0000

    update: tratamento de excessões

--- ex05 - Encapsulamento.py
+++ ex05 - Encapsulamento.py
@@ -4,6 +4,9 @@ os.system("cls || clear") # Limpa o terminal
 class SaldoInsuficienteError(Exception):
     pass
 
+class ValorNegativoError(Exception):
+    pass
+
 class Conta:
     def __init__(self, numero_conta: int, agencia: int) -> None:
         self.numero_conta = numero_conta
@@ -20,18 +23,26 @@ class Conta:
             self.__verificar_sacar(valor)
         except SaldoInsuficienteError as error:
             return f"Erro: {error}"
-
         
         self._saldo -= valor
         return self._saldo
     
-    def __verificar_sacar(self, valor): # etodo privado
+    def __verificar_sacar(self, valor): # Metodo privado
         if valor > self._saldo:
             raise  SaldoInsuficienteError("Saldo insuficiente") # Lançando um erro
         
     def depositar(self, valor):
+        try: 
+            self.__verificar_depositar(valor)
+        except ValorNegativoError as error:
+            return f"Error: {error}"
+
         self._saldo += valor
         return self._saldo
+
+    def __verificar_sacar(self, valor):
+        if valor< 0:
+            raise ValorNegativoError("Não é possivel depositar valor negativo.")
 
 class ContaCorrente(Conta):
     pass
@@ -43,6 +54,13 @@ class ContaPoupanca(Conta):
 conta_corrente = ContaCorrente(222, 333)
 conta_poupanca = ContaPoupanca(444, 555)
 
+# CONTA CORRENTE
 print(conta_corrente._saldo)
 print(conta_corrente.sacar(200))
 print(conta_corrente._saldo)
+print(conta_corrente.depositar(-200))
+
+# CONTA CORRENTE
+# print(conta_poupanca._saldo)
+# print(conta_poupanca.sacar(200))
+# print(conta_poupanca._saldo)
